5039961e34406adb1a1a92e36d5e707c48a9d35d
TB831 <druu831@gmail.com>
2018-07-03 15:04:10 -0700

    App now has a constructur with YTSearch function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar.js';
 
+const API_KEY = 'AIzaSyBLUqAK2TpJJGXdUAkGDucMfgFdq4YyXYU'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (data) => {
+      this.setState({ videos })
+    });
+  }
   render() {
     return (
       <div>
