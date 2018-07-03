3efe73f846e3fbb791851d367bbdf25363df1e32
TB831 <druu831@gmail.com>
2018-07-03 16:20:50 -0700

    VideoList renders VideoListItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,8 +3,13 @@ import React from 'react';
 import VideoListItem from './VideoListItem.js';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem key={video.etag} video={video} />
+{}  })
+
   return (
     <ul className="col-md-4 list-group">
+      {videoItems}
     </ul>
   )
 }
