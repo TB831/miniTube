fda3be2761ca047a1193215ca9d13adf32b62b25
TB831 <druu831@gmail.com>
2018-07-03 16:58:00 -0700

    Video now displays on main app page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar.js';
 import VideoList from './components/VideoList.js';
-
+import VideoDetail from './components/VideoDetail.js';
 import config from '../config.js';
 
 class App extends Component {
@@ -24,9 +24,9 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList
-          videos={ this.state.videos }
-        />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos}/>
+
       </div>
     );
   }
